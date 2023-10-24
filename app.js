4c91b8a50665f33820a9f58554cfc3e1e3f092d7
tztzlk <maaaaaaaksat@gmail.com>
2023-10-25 03:15:59 +0600

    edit

--- app.js
+++ app.js
@@ -1,3 +1,5 @@
+
+
 //функция рандом комп 
 function getComputerChoice(){
     const game = ["Ножницы", "Бумага", "Камень"];
@@ -5,28 +7,5 @@ function getComputerChoice(){
     return game[random]
 }
 
-const computerSelection = getComputerChoice();
 
-function playRound(playerSelection,computerSelection){
-    const player = playerSelection.toLowerCase()
-    const computer = computerSelection.toLowerCase()
-    if(player == computer){
-        return "ничья " + playerSelection    
-    }
-    else if(
-    (player === "Камень" && computer === "Ножницы") ||
-    (player === "Бумага" && computer === "Камень") ||
-    (player === "Ножницы" && computer === "Бумага")
-    ){
-        return("you win " + playerSelection + " beats " + computerSelection)
-    }
-    else{
-       return("you lose! " + computerSelection + " beats " + playerSelection) 
-    }
-}
 
-const playerSelection =  prompt("выберите: Ножницы, Бумага, Камень").trim()  
-    if(playerSelection === "Ножницы" || playerSelection === "Бумага" || playerSelection == "Камень"){
-    const result  =  playRound(playerSelection,computerSelection)
-       console.log(result)
-    }
